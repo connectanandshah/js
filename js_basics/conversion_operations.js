883be81e049d148d466ea5c7cf140d3195dddbea
connectanandshah <151903307+connectanandshah@users.noreply.github.com>
2023-11-29 05:54:46 +0000

    coversion

---
+++ js_basics/conversion_operations.js
@@ -0,0 +1,34 @@
+let score = true
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueNumber = Number(score)
+console.log(typeof valueNumber);
+console.log(valueNumber)
+
+
+
+
+let a = ""
+let boolean_a = Boolean(a)
+console.log(boolean_a)
+
+/*
+true/false = 1
+33 = 3
+33abc = NaN
+Undefined = NaN
+Null = 0
+
+ 1 = true
+ 0 = false
+ "" = false
+ "value" = true
+*/
+
+let num = 33
+let strNum = String(num)
+console.log(strNum);
+
+console.log(typeof strNum);
