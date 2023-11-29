7bbb6a65528404e82f2e129e0e6a044073419998
connectanandshah <151903307+connectanandshah@users.noreply.github.com>
2023-11-29 09:30:53 +0000

    daattoes

--- js_basics/comparison.js
+++ js_basics/comparison.js
@@ -1,1 +1,13 @@
 console.log(2>1);
+
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
+//undefined is always flase while doing comparison //
+
+console.log("2" === 2);
+
+
